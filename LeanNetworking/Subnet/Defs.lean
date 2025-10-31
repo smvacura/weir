4641d3bd5f23c671195a53bb411e8b4febede386
Samuel Vacura <smvacura@gmail.com>
2025-10-30 21:56:10 -0400

    document subnet defs

--- LeanNetworking/Subnet/Defs.lean
+++ LeanNetworking/Subnet/Defs.lean
@@ -2,8 +2,13 @@ import LeanNetworking.Mask.Defs
 import LeanNetworking.Mask.BitVec
 import LeanNetworking.Mask.Theorems
 
+
+/-- Definition of a subnet. A subnet is a set with `ϕ(x) = the subnet mask
+  formed with x and m is equal to the subnet mask formed with a and m`, with
+  `a, m` being parameters-/
 def subnet (a : IP) (m : SubnetMask) : Set IP :=
   fun ip => applySubnetMask ip m = applySubnetMask a m
 
 
+/-- The size of a subnet is `2^n`, where `n` is the length of the network portion-/
 def subnetSize (mask : SubnetMask) := 2^(32-mask.val)
