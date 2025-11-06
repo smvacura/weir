d06d2eb0732f25994297ccbb9824fbb78bb6c3f4
Samuel Vacura <smvacura@gmail.com>
2025-11-05 23:41:17 -0500

    Add IP aggregation for NICs

--- LeanAzure/NIC/Defs.lean
+++ LeanAzure/NIC/Defs.lean
@@ -18,14 +18,23 @@ structure IPConfiguration where
 
 
 
-structure NIC where
+structure AzureNIC where
   -- required attributes
   name : String
   location : String
   rg : String
   ip_configs : List IPConfiguration
+  nonempty_configs : ip_configs ≠ []
 
   -- optional attributes
   dns_servers : List IP
   edges_zone : Unit
   ip_forwarding_enabled : Bool
+
+
+def isStatic (n : IPConfiguration) : Prop := n.private_allocation = PrivateAllocation.Static
+def isDynamic (n : IPConfiguration) : Prop := n.private_allocation ≠  PrivateAllocation.Static
+
+
+def reachableIPs (n : AzureNIC) : Set IP :=
+  List.foldl (λa (b : IPConfiguration) => a ∪ (cidr.toSet b.subnet)) {} n.ip_configs
