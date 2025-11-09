44fb9e5c0f5459c9812b741c93934c0bedcfb092
Samuel Vacura <smvacura@gmail.com>
2025-11-08 19:44:23 -0500

    Add initial NSG def

---
+++ LeanAzure/NSG/Defs.lean
@@ -0,0 +1,43 @@
+import LeanNetworking.CIDR.Defs
+import LeanAzure.Defs
+
+inductive Direction where
+  | Inbound
+  | Outbound
+
+
+inductive Access where
+  | Allow
+  | Deny
+
+-- Tcp, Udp, Icmp, Esp, Ah or *
+inductive Protocol where
+  | TCP
+  | UDP
+  | ICMP
+  | ESP
+  | AH
+  | All
+
+
+inductive Port where
+  | Specific (p : Nat)
+  | All
+
+structure AzureSecurityRule where
+  name : String
+  rule_priority : Nat
+  direction : Direction
+  access : Access
+  protocol : Protocol
+  source_port_range : Port
+  destination_port_range : Port
+  source_address_prefix : CIDR
+  destination_address_prefix : CIDR
+
+
+structure AzureNSG where
+  name : String
+  location : AzureLocation
+  resource_group : AzureResourceGroup
+  tags : List Tag
