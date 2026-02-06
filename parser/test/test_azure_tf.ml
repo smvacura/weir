80f296c0e14a1fe72793299a4708650e724ebd01
Samuel Vacura <smvacura@gmail.com>
2026-02-05 21:53:37 -0500

    Add rudimentary testing

---
+++ parser/test/test_azure_tf.ml
@@ -0,0 +1,21 @@
+open OUnit2
+open Frontends.AzureTF
+open Azureir
+open Parser.Azure_types
+
+
+let single_rg_world = 
+  let world = World.empty in
+  let rg = Rg.make_rg 
+    "example-resources"
+    (Rg.Id.of_string "/subscriptions/12345678-1234-1234-1234-123456789abc/resourceGroups/example-resources")
+    EastUs
+    ""
+    []
+  in
+  let rgs' = Rg.Map.add (Rg.get_name rg) rg world.resource_groups in
+  { world with resource_groups = rgs' }
+
+let basic_tests = "test suite for simple graphs" >::: [
+  "single_rg" >:: (fun _ -> assert_equal single_rg_world (AzureTFParser.get_resources "test_plans/single_rg.json"))
+]
