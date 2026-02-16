f0169303f750d5d45dd282ece5df0a9b8ecef3fb
Samuel Vacura <smvacura@gmail.com>
2026-02-15 23:10:16 -0500

    Add NSG definition

--- parser/lib/AzureIR/nsg.ml
+++ parser/lib/AzureIR/nsg.ml
@@ -1,13 +1,31 @@
 open Parser.Azure_types
 
 module Id = struct
-  type t = string
+  
+  type t = string * string * string
 
-  let compare = String.compare
+  let compare = compare
+
+  let of_strings sub rg name : t = (sub, rg, name)
+
+  let to_strings ((sub, rg, name) : t )  = (sub, rg, name)
+end
+
+module SecurityRule = struct
+  
+  type t = {
+    name: string;
+  }
 end
 
 type t = {
-    name : string
+    name : string;
+    subscription : string;
+    address : string;
+    location : azure_location;
+    resource_group : Rg.t;
+    rule_list : SecurityRule.t list;
+    tags : tag list;
   }
 
 module Map = Map.Make(Id)
--- parser/lib/AzureIR/nsg.mli
+++ parser/lib/AzureIR/nsg.mli
@@ -1,11 +1,17 @@
-
 module Id : sig
     type t
 
     val compare : t -> t -> int
 
+    val of_strings : string -> string -> string -> t
+
+    val to_strings : t -> (string * string * string)
+
 end
 
+module SecurityRule : sig
+  type t
+end
 
 type t
 
