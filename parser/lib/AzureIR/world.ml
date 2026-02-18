863584cb7cc8ebf73804334995b40fd3709d5163
Samuel Vacura <smvacura@gmail.com>
2026-02-17 21:04:09 -0500

    Add NSG to world

--- parser/lib/AzureIR/world.ml
+++ parser/lib/AzureIR/world.ml
@@ -2,7 +2,8 @@
 type t = {
   resource_groups : Rg.t Rg.Map.t;
   subnets : Subnet.t Subnet.Map.t;
-  vnets : Vnet.t Vnet.Map.t
+  vnets : Vnet.t Vnet.Map.t;
+  nsgs : Nsg.t Nsg.Map.t;
 }
 
 let equal t1 t2 =
@@ -13,7 +14,8 @@ let equal t1 t2 =
 let empty = {
   resource_groups = Rg.Map.empty;
   subnets = Subnet.Map.empty;
-  vnets = Vnet.Map.empty
+  vnets = Vnet.Map.empty;
+  nsgs = Nsg.Map.empty;
 }
 
 let get_resource_group world subscription name = 
@@ -22,4 +24,5 @@ let get_resource_group world subscription name =
 let show world = 
   "Resource groups: " ^ Rg.show_rg_map world.resource_groups ^ "\n" ^
   "Vnets: " ^ Vnet.show_vnet_map world.vnets ^ "\n" ^
-  "Subnets: " ^ Subnet.show_subnet_map world.subnets ^ "\n"
+  "Subnets: " ^ Subnet.show_subnet_map world.subnets ^ "\n" ^
+  "Nsgs: " ^ Nsg.show_nsg_map world.nsgs ^ "\n"
--- parser/lib/AzureIR/world.mli
+++ parser/lib/AzureIR/world.mli
@@ -2,7 +2,8 @@
 type t = {
   resource_groups : Rg.t Rg.Map.t;
   subnets : Subnet.t Subnet.Map.t;
-  vnets : Vnet.t Vnet.Map.t
+  vnets : Vnet.t Vnet.Map.t;
+  nsgs : Nsg.t Nsg.Map.t
 }
 
 val empty : t
