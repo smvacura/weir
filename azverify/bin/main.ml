aa825f626c02ef0f237a06217d5bf8f889319cd9
Samuel Vacura <smvacura@gmail.com>
2026-05-27 22:58:23 -0400

    remove old SMT files

--- azverify/bin/main.ml
+++ azverify/bin/main.ml
@@ -1,21 +1,4 @@
-open Subprocess
 
-open Parser.Smt_ir
-open Parser.Smt_to_file
-
-let test_ir = [
-  SetLogic "QF_BV";
-  DeclareConst ("x", BitVec 8);
-  DeclareConst ("y", BitVec 8);
-  Assert (Eq (Symbol "x", BV (8, 5)));      (* x = #b00000101 *)
-  Assert (Eq (Symbol "y", Symbol "x"));     (* y = x           *)
-  CheckSat;
-  GetModel
-]
 
 let () = 
-  let filename = "test.smt2" in
-  let z3_cmd = cmd ["z3"; filename] in
-  smt_to_file test_ir filename;
-  let model = Subprocess.read z3_cmd in
-  print_string model;
+  print_string "hello_world"
--- azverify/parser/smt_to_file.mli
+++
@@ -1,3 +0,0 @@
-val smt_term_to_string : Smt_ir.smt_term -> string
-
-val smt_to_file : Smt_ir.smt_cmd list -> string -> unit
